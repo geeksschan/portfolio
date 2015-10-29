ffd069f3bab8f819897541c45d9ef2a6b5dd5e32
geeksschan <geeksschan@gmail.com>
2015-10-29 17:20:37 +0900

    페이지 이동시 로딩 화면 표시 추가

--- public/assets/js/default.js
+++ public/assets/js/default.js
@@ -1,3 +1,18 @@
+window.onload = function() {
+    var $body = $("body"),
+        $init_loading = $("#init-loading"),
+        $loading_div = $init_loading.find("div"),
+        $loading_div_height = $loading_div.height();
+
+    $init_loading.css("height", 0).css("top", -$loading_div_height+"px");
+
+    setTimeout(function() {
+        console.log("test");
+        $loading_div.css("display", "none");
+        $body.removeClass("loading");
+    }, 3000);
+};
+
 $(function() {
 
     // drawer menu open/close
@@ -26,7 +41,4 @@ $(function() {
 
         }
     });
-
-
-
 });
