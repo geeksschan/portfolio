770ccc83ab9e11d14c967d02632d9ae0fd68440b
geeksschan <geeksschan@gmail.com>
2015-12-16 11:16:53 +0900

    board 라는 이름을 blog로 일괄 변경

--- public/assets/js/blog.js
+++ public/assets/js/blog.js
@@ -3,21 +3,21 @@ $(function() {
     var $window = $(window),
         $window_height = $window.height(),
         $window_width = $window.width(),
-        $board_sidebar = $("#board-sidebar"),
-        $board_sidebar_content = $board_sidebar.find(".board-sidebar-content"),
-        $board_sidebar_content_height = $board_sidebar_content.height(),
-        $board_sidebar_btn_wrapper = $(".sidebar-btn-wrapper"),
-        $board_sideber_btn = $(".board-sidebar-btn"),
-        $board_body = $("#board-body"),
-        $board_list = $board_body.find(".board-list"),
-        $blog_content = $board_body.find(".blog-content"),
-        $board_list_height = $board_list.height(),
-        $window_sidebar_gap = $board_sidebar_content_height - $window_height, // 사이드 메뉴 높이가 윈도우 창 보다 높은지 여부
-        $board_backdrop = $("#board-backdrop"),
+        $blog_sidebar = $("#blog-sidebar"),
+        $blog_sidebar_content = $blog_sidebar.find(".blog-sidebar-content"),
+        $blog_sidebar_content_height = $blog_sidebar_content.height(),
+        $blog_sidebar_btn_wrapper = $(".sidebar-btn-wrapper"),
+        $blog_sideber_btn = $(".blog-sidebar-btn"),
+        $blog_body = $("#blog-body"),
+        $blog_list = $blog_body.find(".blog-list"),
+        $blog_content = $blog_body.find(".blog-content"),
+        $blog_list_height = $blog_list.height(),
+        $window_sidebar_gap = $blog_sidebar_content_height - $window_height, // 사이드 메뉴 높이가 윈도우 창 보다 높은지 여부
+        $blog_backdrop = $("#blog-backdrop"),
         current_scroll_top = 0,
         last_scroll_top = 0,
         scroll_direction = 0,
-        fixed_top = 0, // 모바일 사이드 메뉴 열림 시 board_list 고정 시키거나 닫을 때 다시 재조정해 줘야 하는 높이 값
+        fixed_top = 0, // 모바일 사이드 메뉴 열림 시 blog_list 고정 시키거나 닫을 때 다시 재조정해 줘야 하는 높이 값
         direction_change = false, // 방향 변경 여부
         $summernote = $("#summernote"),
         $summernote_initial_height = 500,
@@ -58,37 +58,37 @@ $(function() {
      * 메뉴 사이드 바에 따른 스크롤 이벤트
      */
     if($window_sidebar_gap > 0) {
-        if($board_list_height > $board_sidebar_content_height) {
+        if($blog_list_height > $blog_sidebar_content_height) {
 
             $window.scroll(function() {
 
                 if($window_sidebar_gap <= $window.scrollTop()) {
                     if(scroll_direction == 1) {
                         if(direction_change) {
-                            $board_sidebar_content.css("position","relative").css("bottom", "initial").css("top", -$window_sidebar_gap + "px");
+                            $blog_sidebar_content.css("position","relative").css("bottom", "initial").css("top", -$window_sidebar_gap + "px");
                         }
                     } else {
-                        $board_sidebar.css("position", "fixed").css("top", 0);
-                        $board_sidebar_content.css("position","fixed").css("top", "initial").css("bottom", "0");
+                        $blog_sidebar.css("position", "fixed").css("top", 0);
+                        $blog_sidebar_content.css("position","fixed").css("top", "initial").css("bottom", "0");
                     }
                 } else {
-                    //$board_sidebar.css("position", "absolute").css("top", 0);
-                    $board_sidebar_content.css("position","relative").css("top",0);
+                    //$blog_sidebar.css("position", "absolute").css("top", 0);
+                    $blog_sidebar_content.css("position","relative").css("top",0);
                 }
 
             });
         }
     } else {
         // fixed
-         $board_sidebar.css("position","fixed").css("top", "0");
+         $blog_sidebar.css("position","fixed").css("top", "0");
     }
 
     /**
      * 메뉴 사이드 바 열림/닫기 버튼
      */
-    $board_sideber_btn.on("click", function() {
+    $blog_sideber_btn.on("click", function() {
 
-        var fixed_top_plus = parseInt($board_sidebar_btn_wrapper.height());
+        var fixed_top_plus = parseInt($blog_sidebar_btn_wrapper.height());
 
         // 닫기
         if($(this).hasClass("open")) {
@@ -96,10 +96,10 @@ $(function() {
                 $submit_btn_wrapper.removeClass("close");
             }
 
-            $board_sidebar_btn_wrapper.removeClass("open");
+            $blog_sidebar_btn_wrapper.removeClass("open");
             $(this).removeClass("open");
-            $board_sidebar.removeClass("open");
-            $board_backdrop.removeClass("open");
+            $blog_sidebar.removeClass("open");
+            $blog_backdrop.removeClass("open");
 
             fixed_top -= fixed_top_plus;
             if(fixed_top >= 0) {
@@ -116,18 +116,18 @@ $(function() {
                 $submit_btn_wrapper.addClass("close");
             }
 
-            $board_sidebar.addClass("open");
-            $board_sidebar_btn_wrapper.addClass("open");
+            $blog_sidebar.addClass("open");
+            $blog_sidebar_btn_wrapper.addClass("open");
             $(this).addClass("open");
-            $board_backdrop.addClass("open");
+            $blog_backdrop.addClass("open");
 
             fixed_top = fixed_top_plus - last_scroll_top;
             $blog_content.css("position", "fixed").css("top", fixed_top+"px");
         }
     });
 
-    $board_backdrop.on("click", function() {
-        $board_sideber_btn.trigger("click");
+    $blog_backdrop.on("click", function() {
+        $blog_sideber_btn.trigger("click");
     });
 
 
